662fe4a4086aafda6c25a14cad97669ee06835bb
Gertt <gert@gert.gert>
2016-11-30 01:00:15 +0100

    code style update

--- tests/setup.js
+++ tests/setup.js
@@ -4,13 +4,13 @@ global.document = jsdom('<div id="app"></div>');
 global.window = document.defaultView;
 
 Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    global[property] = document.defaultView[property];
-  }
+    if (typeof global[property] === 'undefined') {
+        global[property] = document.defaultView[property];
+    }
 });
 
 global.navigator = {
-  appName: 'other',
-  userAgent: 'node.js',
-  platform: 'node.js',
+    appName: 'other',
+    userAgent: 'node.js',
+    platform: 'node.js',
 };
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -3,16 +3,16 @@
 var webpack = require('webpack');
 
 module.exports = {
-  module: {
-    loaders: [
-      { test: /\.jsx?$/, loaders: ['babel-loader'], exclude: /node_modules/ }
-    ]
-  },
-  output: {
-    library: 'ReactAce',
-    libraryTarget: 'umd'
-  },
-  resolve: {
-    extensions: ['', '.js']
-  }
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, loaders: ['babel-loader'], exclude: /node_modules/ }
+        ]
+    },
+    output: {
+        library: 'ReactAce',
+        libraryTarget: 'umd'
+    },
+    resolve: {
+        extensions: ['', '.js']
+    }
 };
