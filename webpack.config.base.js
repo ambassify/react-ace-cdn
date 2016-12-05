7ef6247f066aea32e09c64586832f8d23f5f4c3a
Gertt <gert@gert.gert>
2016-12-05 19:51:10 +0100

    Exclude React from dist build
    The most logical use-case of the dist build would be to use it in a
    react project that uses the CDN version of react instead of browserify
    or webpack. If this is the case, including it in the build will result
    in multiple instances of react which causes errors.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,6 +12,14 @@ module.exports = {
         library: 'ReactAce',
         libraryTarget: 'umd'
     },
+    externals: {
+        'react': {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        }
+    },
     resolve: {
         extensions: ['', '.js']
     }
